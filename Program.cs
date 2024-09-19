dd736548b636a4fce75d2f28f590dbbca74711a0
larsg <lars.gunnar@jobloop.no>
2024-09-19 13:29:05 +0200

    expand comments

--- Program.cs
+++ Program.cs
@@ -2,24 +2,34 @@ internal class Program
 {
   private static void Main(string[] args)
   {
-    // Builder Pattern
+    // The web server is a complex piece of software
+    // So it's constructed in a two step process
+    // called the builder pattern
+    // https://en.wikipedia.org/wiki/Builder_pattern
     var builder = WebApplication.CreateBuilder(args);
     var app = builder.Build();
 
-    // Middleware for serving static files from the "wwwroot" directory
+    // Middleware for serving "index.html" at "/", rather then "/index.html"
     app.UseDefaultFiles();
+    // Middleware for serving static files from the "wwwroot" directory
     app.UseStaticFiles();
 
+    // You can setup individual functions here
+    // that should be executed when called and return som value
+
+    // GET localhost:xxxx/health -> "Server OK"
     app.MapGet("/health", () =>
     {
       return "Server OK!";
     });
 
+    // POST localhost:xxxx/api/v1/todoes -> "Recievd a POST Request to '/api/v1/todoes'"
     app.MapPost("/api/v1/todos", () =>
     {
-      return "Recived a POST Request to '/'";
+      return "Recived a POST Request to '/api/v1/todoes'";
     });
 
+    // After configuration, then start the application
     app.Run();
   }
 }
